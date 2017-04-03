00a4a2a12e560312f794105a355856bc927d1632
reedts <j.reedts@gmail.com>
2017-04-03 10:31:54 +0200

    Added doc

--- store/store_io.h
+++ store/store_io.h
@@ -7,6 +7,14 @@ extern "C" {
 
 struct device_list;
 
+/**
+ * @brief Stores a device list persistently
+ *
+ * @param list Device list to store
+ * 
+ * @retval 0 The device list was stored successfully
+ * @retval -1 An error occurred and `errno` was set to indicate the cause
+ */
 int omemo_store_device_list(struct device_list *list);
 
 #ifdef __cplusplus
