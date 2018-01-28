71b862c33303402a24658a0af160180f8f10c5c7
reedts <j.reedts@gmail.com>
2018-01-28 18:35:12 +0100

    Further defined context type

--- structs/omemo_context.c
+++ structs/omemo_context.c
@@ -5,6 +5,21 @@
 #include <store/omemo_store.h>
 #include "omemo_context.h"
 
+#define UNUSED(x) (void)(x)
+
+static void omemo_lock(void *user_data)
+{
+	struct omemo_context *context = user_data;
+
+	pthread_mutex_lock(&context->mutex);
+}
+
+static void omemo_unlock(void *user_data) 
+{
+	struct omemo_context *context = user_data;
+
+	pthread_mutex_unlock(&context->mutex);
+}
 
 struct omemo_context *omemo_context_create(const signal_protocol_address *address)
 {
@@ -23,55 +38,57 @@ struct omemo_context *omemo_context_create(const signal_protocol_address *addres
 	}
 	
 	memcpy(&context->own_address, address, sizeof(signal_protocol_address));
-	context->store_context = &omemo_store_context;
-
-	/* Check if the user's context exists already */
-	if (!omemo_is_local_user_existent(address)) {
-		/* Install OMEMO, a.k.a generate Keys etc. */
+	context->store_context = omemo_store_context_create(context);
+	if (!context->store_context) {
+		return NULL;
 	}
 
-	retval = signal_context_create(&context->signal_context, NULL);
+	retval = signal_context_create(&context->signal_ctx, context);
 	if (retval < 0) {
 		errno = ENOMEM;
 		return NULL;
 	}
 
-	retval = signal_context_set_crypto_provider(context->signal_context, &omemo_crypto_provider);
-	if (retval < 0) {
-		return NULL;
-	}
-
-	/* TODO: Set signal locking functions */
-
-	retval = signal_protocol_store_context_create(&context->signal_store_context, context->signal_context);
-	if (retval < 0) {
-		return NULL;
-	}
-
-	retval = signal_protocol_store_context_set_session_store(context->signal_store_context,
-			&omemo_store_context.session_store);
-	if (retval < 0) {
-		return NULL;
-	}
-
-	retval = signal_protocol_store_context_set_pre_key_store(context->signal_store_context,
-			&omemo_store_context.pre_key_store);
-	if (retval < 0) {
-		return NULL;
-	}
-
-	retval = signal_protocol_store_context_set_signed_pre_key_store(context->signal_store_context,
-			&omemo_store_context.signed_pre_key_store);
-	if (retval < 0) {
-		return NULL;
-	}
-
-	retval = signal_protocol_store_context_set_identity_key_store(context->signal_store_context,
-			&omemo_store_context.identity_key_store);
+	retval = signal_context_set_crypto_provider(context->signal_ctx, &omemo_crypto_provider);
 	if (retval < 0) {
 		return NULL;
 	}
 	
+	pthread_mutex_init(&context->mutex, NULL);
+	retval = signal_context_set_locking_functions(context->signal_ctx, omemo_lock, omemo_unlock);
+	if (retval < 0) {
+		return NULL;
+	}
+
+	retval = signal_protocol_store_context_create(&context->signal_store_ctx, context->signal_ctx);
+	if (retval < 0) {
+		return NULL;
+	}
+
+	retval = signal_protocol_store_context_set_session_store(context->signal_store_ctx,
+			&context->store_context->session_store);
+	if (retval < 0) {
+		return NULL;
+	}
+
+	retval = signal_protocol_store_context_set_pre_key_store(context->signal_store_ctx,
+			&context->store_context->pre_key_store);
+	if (retval < 0) {
+		return NULL;
+	}
+
+	retval = signal_protocol_store_context_set_signed_pre_key_store(context->signal_store_ctx,
+			&context->store_context->signed_pre_key_store);
+	if (retval < 0) {
+		return NULL;
+	}
+
+	retval = signal_protocol_store_context_set_identity_key_store(context->signal_store_ctx,
+			&context->store_context->identity_key_store);
+	if (retval < 0) {
+		return NULL;
+	}
+
 	return context;
 }
 
@@ -81,8 +98,9 @@ void omemo_context_free(struct omemo_context *context)
 		return;
 	}
 
-	signal_protocol_store_context_destroy(context->signal_store_context);
-	signal_context_destroy(context->signal_context);
-
+	signal_protocol_store_context_destroy(context->signal_store_ctx);
+	signal_context_destroy(context->signal_ctx);
+	pthread_mutex_destroy(&context->mutex);
+	free(context->store_context);
 	free(context);
 }
--- structs/omemo_context.h
+++ structs/omemo_context.h
@@ -1,21 +1,23 @@
 #ifndef PROF_OMEMO_CONTEXT_H
 #define PROF_OMEMO_CONTEXT_H
 
+#include <pthread.h>
 #include <signal_protocol_types.h>
 
-#ifdef __cpluslus
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 struct omemo_context {
 	signal_protocol_address own_address;
-	signal_context *signal_context;
-	signal_protocol_store_context *signal_store_context;
+	signal_context *signal_ctx;
+	signal_protocol_store_context *signal_store_ctx;
 	
 	struct omemo_store_context *store_context;
 	struct omemo_bundle *bundle;
+	
+	pthread_mutex_t mutex;
 };
-
 
 struct omemo_context *omemo_context_create(const signal_protocol_address *address);
 
@@ -27,4 +29,4 @@ void omemo_context_free(struct omemo_context *context);
 }
 #endif
 
-#endif /* PROF_OMEMO_DEVICE_H */
+#endif /* PROF_OMEMO_CONTEXT_H */
