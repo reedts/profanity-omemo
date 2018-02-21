1b0d01df9325ef61e98c9dc106959cdbb3fb4a48
Roberto Santalla <roobre@roobre.es>
2018-02-21 15:51:03 +0100

    Changed function name according to header

--- xmpp/omemo_stanza.c
+++ xmpp/omemo_stanza.c
@@ -4,7 +4,7 @@
 
 #include <xmpp/omemo_stanza.h>
 
-omemo_stanza_type is_omemo_stanza(const char *stanza)
+omemo_stanza_type omemo_check_stanza_type(const char *stanza)
 {
 	// TODO
 	return OMEMO_STYPE_NONE;
