0f954d7a7d737b544f622ecf6166cac53bf3cd42
reedts <j.reedts@gmail.com>
2018-02-21 15:04:14 +0100

    Fixed include of omemo_constants

--- test/test_main.h
+++ test/test_main.h
@@ -7,11 +7,11 @@
 #include <fstream>
 
 #include <crypto/crypto_provider.h>
+#include <omemo/omemo_constants.h>
 #include <store/omemo_store.h>
 #include <structs/device_list.h>
 #include <structs/omemo_device.h>
 #include <xmpp/pubsub.h>
-#include <xmpp/omemo_constants.h>
 
 int main(int argc, char **argv)
 {
